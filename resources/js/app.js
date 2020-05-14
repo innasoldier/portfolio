0716748074174339dc5efa44aecc0690350581ff
inna_soldier <inna.soldier@gmail.com>
2020-05-13 19:17:41 -0500

    updates, php artisan auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-import Buefy from 'buefy';
 
 Vue.use(Buefy);
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
